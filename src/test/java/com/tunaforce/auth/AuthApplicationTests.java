37ab49f27fab47c87b83e143f68ff51703984782
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2025-08-14 10:20:37 +0900

    Init: Initialize project

---
+++ src/test/java/com/tunaforce/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tunaforce.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
