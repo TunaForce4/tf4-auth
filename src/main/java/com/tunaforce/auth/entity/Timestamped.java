ffe197784e29b340cf279d877a29f617ed4c834f
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2025-08-15 14:30:52 +0900

    fix(auth): rename timestamped entity delete method

--- src/main/java/com/tunaforce/auth/entity/Timestamped.java
+++ src/main/java/com/tunaforce/auth/entity/Timestamped.java
@@ -1,14 +1,12 @@
 package com.tunaforce.auth.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
@@ -17,22 +15,21 @@ import java.util.UUID;
 public abstract class Timestamped {
     @CreatedDate
     @Column(name = "created_at", updatable = false, nullable = false)
-    protected Instant createdAt;
+    protected LocalDateTime createdAt; // 한국내에서만 하는 서비스라 LocalDateTime으로 설정
 
     @LastModifiedDate
     @Column(name = "updated_at", nullable = false)
-    protected Instant updatedAt;
+    protected LocalDateTime updatedAt;
 
     @Column(name = "deleted_at")
-    protected Instant deletedAt;
+    protected LocalDateTime deletedAt;
 
     @Column(name = "deleted_by")
     protected UUID deletedBy;
 
     // 삭제 메서드
-    public void markDeleted(UUID userId) {
-        this.deletedAt = Instant.now();
+    public void delete(UUID userId) {
+        this.deletedAt = LocalDateTime.now();
         this.deletedBy = userId;
     }
-
 }
