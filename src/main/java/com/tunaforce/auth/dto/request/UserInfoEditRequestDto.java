a96db0b8ed0f1bdf93404157840acbf7d5b5d84d
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2025-08-20 23:30:43 +0900

    refactor: fix dto userrole type

--- src/main/java/com/tunaforce/auth/dto/request/UserInfoEditRequestDto.java
+++ src/main/java/com/tunaforce/auth/dto/request/UserInfoEditRequestDto.java
@@ -1,9 +1,11 @@
 package com.tunaforce.auth.dto.request;
 
+import com.tunaforce.auth.entity.UserRole;
+
 public record UserInfoEditRequestDto(
         String name,
         String password,
-        String role,
+        UserRole role,
         String slackId,
         String tel
 ) {
